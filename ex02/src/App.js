3acb8623875a7ea8c669d87b6347700ba0c20b08
Safijaz <safija.zahirovic@gmail.com>
2021-03-27 01:08:48 +0100

    ex02

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         <code> Hello Padawans!</code>
-        </p>
+        <p>Hello Padawans!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
